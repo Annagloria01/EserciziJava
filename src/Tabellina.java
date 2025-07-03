e4e14c56f887d653ce11a072bd953f9e6779d709
Annagloria <provanorenna@gmail.com>
2025-07-03 20:42:13 +0200

    tabellina

---
+++ src/Tabellina.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Tabellina {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci numero di una tabellina");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Inserisci numero al quale la tabellina deve arrivare");
+        int numeroFinetabellina = scanner.nextInt();
+
+        for (int i = 0; i <= numeroFinetabellina; i++) {
+            System.out.println("Ecco il tuo prodotto:" + (numero * i));
+            
+        }
+    }
+}
