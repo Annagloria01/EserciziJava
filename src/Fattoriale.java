3fe1e2cd3eef8b4289472db81d38a0125aa81349
Annagloria <provanorenna@gmail.com>
2025-07-07 20:06:00 +0200

    fattoriale

---
+++ src/Fattoriale.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        //fattoriale
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci numero da cui calcolare il fattoriale");
+
+        int numeroInserito = scanner.nextInt();
+
+        //num fattoriale = n * (n-1) * (n-2) * ... 1
+        int fattoriale = numeroInserito;
+        
+        String result = numeroInserito + "";
+
+        for (int i = 1; i < numeroInserito; i++) {
+            fattoriale *= (numeroInserito - i);
+            result += "x" +  (numeroInserito - i);
+        }
+
+        System.out.println(result + " = " + fattoriale);
+
+    }
+}
